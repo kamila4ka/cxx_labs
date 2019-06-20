d5b4bf1c73b2e4422fb591f3f2e6d754381d3469
kamila4ka <48149585+kamila4ka@users.noreply.github.com>
2019-06-21 00:51:06 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+class example
+{
+public:
+    example(){}
+    example(int a, int b): _a(a), _b(b){}
+    int _a;
+    int _b;
+};
+
+int main()
+{
+    List<int> spisok1;
+
+    for(int i=0; i<4; i++)
+            spisok1.append(i);
+    spisok1.addbegin(22);
+    spisok1.add(9, 3);
+    spisok1.del(3);
+
+
+    cout << "count: " << spisok1.getCount() << endl << "the first list: ";
+    spisok1.getData();
+    cout << endl << endl;
+
+    spisok1.clear();
+    cout << "cleared. count: " << spisok1.getCount() << endl;
+
+    return 0;
+}
